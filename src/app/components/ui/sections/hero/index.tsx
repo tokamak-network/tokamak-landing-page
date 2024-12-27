5e3b60b870f9c32a6e4e1b2f2e15cc2337b278d3
SonYoungsung <sysgigigi@gmail.com>
2024-12-27 13:58:40 +0900

    fix: revert animation hovering effect

--- src/app/components/ui/sections/hero/index.tsx
+++ src/app/components/ui/sections/hero/index.tsx
@@ -21,13 +21,13 @@ export const Hero: React.FC = () => {
 
   // 자동 애니메이션 설정
   React.useEffect(() => {
-    // if (isHovering) {
-    //   if (intervalRef.current) {
-    //     clearInterval(intervalRef.current);
-    //     intervalRef.current = null;
-    //   }
-    //   return;
-    // }
+    if (isHovering) {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }
+      return;
+    }
     const centerIndex = 579; // 중심 셀
     const row = Math.floor(centerIndex / 40);
     const col = centerIndex % 40;

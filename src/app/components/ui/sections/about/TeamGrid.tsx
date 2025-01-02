5ec58f4d1cec0a44bfb755dd48cbc1e8603386b8
SonYoungsung <sysgigigi@gmail.com>
2025-01-02 16:53:20 +0900

    fix(about): revert grid style

--- src/app/components/ui/sections/about/TeamGrid.tsx
+++ src/app/components/ui/sections/about/TeamGrid.tsx
@@ -176,7 +176,7 @@ const teamMembers = [
 const TeamGrid: React.FC = () => {
   return (
     <div className="w-full h-full flex justify-center [@media(max-width:650px)]:px-[10px]">
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-[1000px] gap-[25px] items-start overflow-x-hidden [-ms-overflow-style:none] [scrollbar-width:non]">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-[1000px] gap-[25px] items-start">
         {teamMembers.map((member, index) => (
           <TeamMember
             key={index}
